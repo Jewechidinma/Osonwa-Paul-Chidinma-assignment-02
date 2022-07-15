2ea8222c83becab7933eb39c0888de3655faeac2
Osonwa-Paul Chidinma <jewelchidinma@gmail.com>
2022-07-15 21:33:26 +0000

    Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let totalValue = 0;
+
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    totalValue += arrayOfNumbers[i];
+  }
+  return totalValue;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +34,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for(const element of arrayOfNumbers) {
+    if (element % 2 === 0) {
+      count++;
+    }
+  } 
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
